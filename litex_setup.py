511614c0a699dc41735e0d2b943d91e1a83c7e1a
Henry <henrydang@fossil.com>
2019-06-11 15:25:46 +0700

    Fixed litex checkout custom branch

--- litex_setup.py
+++ litex_setup.py
@@ -36,8 +36,8 @@ if "init" in sys.argv[1:]:
         # clone repo (recursive if needed)
         print("[cloning " + name + "]...")
         full_url = url + name
-        opts = "--recursive" if need_recursive else ""
-        os.system("git clone " + full_url + " " + opts)
+        branch = "-b custom" if name in ["litex"] else ""
+        os.system("git clone " + full_url + " " + opts + " " + branch)
 
 if "install" in sys.argv[1:]:
     for name in repos.keys():
