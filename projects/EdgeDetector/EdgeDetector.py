3ed00ff62839ae3ad8312c63d87b6f92d9d1d2e8
kamejoko80 <phuongminh.dang@gmail.com>
2019-07-12 00:09:38 +0700

    Update projects

--- projects/EdgeDetector/EdgeDetector.py
+++ projects/EdgeDetector/EdgeDetector.py
@@ -65,13 +65,13 @@ class ShifterIn(Module):
         fsm = FSM(reset_state = "IDLE")
         edt = EdgeDetector()
 
-        self.submodules += fsm, edt
+        self.submodules += fsm #, edt
 
-        self.comb += [
-            edt.s.eq(self.sck),     # Signal input
-            self.rising.eq(edt.r),  # Rising edege detecting input
-            self.falling.eq(edt.f), # Falling edge detecting output
-        ]
+        #self.comb += [
+        #    edt.s.eq(self.sck),     # Signal input
+        #    self.rising.eq(edt.r),  # Rising edege detecting input
+        #    self.falling.eq(edt.f), # Falling edge detecting output
+        #]
 
         fsm.act("IDLE",
             If(self.start == 1,
@@ -80,6 +80,10 @@ class ShifterIn(Module):
             )
         )
         fsm.act("SHIFTING",
+            If(self.cnt == 8,
+                NextValue(self.done, 1),
+                NextState("IDLE"),
+            ),
             If(self.rising,
                 If(self.cnt < 8,
                     NextValue(self.cnt, self.cnt + 1),
@@ -88,10 +92,7 @@ class ShifterIn(Module):
                     ).Else(
                         NextValue(self.dout[0], 0),
                     )                    
-                ).Else(
-                    NextValue(self.done, 1),
-                    NextState("IDLE"),
-                ) 
+                )
             ),
             If(self.falling,
                 If(self.cnt < 8,
@@ -102,9 +103,9 @@ class ShifterIn(Module):
 
         self.sync += [
             If(self.rising, self.start.eq(0)),
-            If(self.falling, self.done.eq(0))            
+            If(self.falling, self.done.eq(0))
         ]
-        
+
 class ShifterOut(Module):
     def __init__(self):
         self.sck     = Signal()
@@ -114,19 +115,19 @@ class ShifterOut(Module):
         self.falling = Signal()
         self.done    = Signal()
         self.din     = Signal(8)
-        self.cnt     = Signal(4) 
+        self.cnt     = Signal(4)
 
         fsm = FSM(reset_state = "IDLE")
         edt = EdgeDetector()
-        
-        self.submodules += fsm, edt
 
-        self.comb += [
-            edt.s.eq(self.sck),     # Signal input
-            self.rising.eq(edt.r),  # Rising edege detecting input
-            self.falling.eq(edt.f), # Falling edge detecting output
-        ]        
-        
+        self.submodules += fsm #, edt
+
+        #self.comb += [
+        #    edt.s.eq(self.sck),     # Signal input
+        #    self.rising.eq(edt.r),  # Rising edege detecting input
+        #    self.falling.eq(edt.f), # Falling edge detecting output
+        #]
+
         fsm.act("IDLE",
             If(self.start == 1,
                 NextValue(self.cnt, 0x00),
@@ -134,13 +135,17 @@ class ShifterOut(Module):
             )
         )
         fsm.act("SHIFTING",
+            If(self.cnt == 8,
+                NextValue(self.done, 1),
+                NextValue(self.so, 0),
+                NextState("IDLE"),
+            ),
             If(self.rising,
                 If(self.cnt < 8, # Hold
                     NextValue(self.cnt, self.cnt + 1),
-                    NextValue(self.din, self.din << 1)                    
+                    NextValue(self.din, self.din << 1)
                 ).Else(
-                    NextValue(self.so, 0),
-                    NextState("IDLE"),
+                    NextValue(self.so, 0)
                 ) 
             ),
             If(self.falling,
@@ -149,42 +154,58 @@ class ShifterOut(Module):
                         NextValue(self.so, 1),
                     ).Else(
                         NextValue(self.so, 0),
-                    )     
-                ).Elif(self.cnt == 8, 
-                    NextValue(self.done, 1),    
+                    )
                 )
             )
         )
 
         self.sync += [
             If(self.falling, self.start.eq(0)),
-            If(self.rising, self.done.eq(0))
+            If(self.falling, self.done.eq(0))
         ]
 
-# Shifter Out + Edge Detector
-class SOWAD(Module):
-    def __init__(self):       
+class CascadingShifter(Module):
+    def __init__(self):
         self.sck     = Signal()
         self.so      = Signal()
+        self.si      = Signal()
+        self.din     = Signal(8)
+        self.dout    = Signal(8)
         self.start   = Signal()
         self.done    = Signal()
-        self.din     = Signal(8)
-        
+
         edt = EdgeDetector()
+        sti = ShifterIn()
         sto = ShifterOut()
-        
-        self.submodules += edt, sto
-        
-        
-        
-        
+        self.submodules += edt, sti, sto
+
+        self.comb += [
+            edt.s.eq(self.sck),
+            sti.rising.eq(edt.r),
+            sti.falling.eq(edt.f),
+            sto.rising.eq(edt.r),
+            sto.falling.eq(edt.f),
+            sti.sck.eq(self.sck),
+            sto.sck.eq(self.sck),
+            sti.si.eq(self.si),
+            self.so.eq(sto.so),
+            sti.start.eq(self.start),
+        ]
+
+        self.sync += [
+            If(edt.r, self.start.eq(0)),
+            sto.start.eq(sti.done),
+            If(sti.done, sto.din.eq(sti.dout)),
+            self.done.eq(sto.done),
+        ]
+
 class TestEdgeDetector(Module):
     def __init__(self):
         self.inp     = Signal() # Signal input
         self.rising  = Signal() # Rising edge detect
         self.falling = Signal() # Falling edge detect
 
-        # Create new debouncer and edge detect objects    
+        # Create new debouncer and edge detect objects
         debouncer = Debouncer()
         edgedetect = EdgeDetector()
         
@@ -196,8 +217,8 @@ class TestEdgeDetector(Module):
             edgedetect.s.eq(debouncer.o),
             self.rising.eq(edgedetect.r),
             self.falling.eq(edgedetect.f)
-        ]        
-        
+        ]
+
 def ShifterInGenerator(dut):
     cnt1 = 0
     cnt2 = 0
@@ -215,8 +236,7 @@ def ShifterInGenerator(dut):
         else:
             cnt2 = 0
             yield dut.si.eq(randrange(2))
-                
-                
+
         if cycle > 2 and cycle < 4:
             yield dut.start.eq(1)
 
@@ -249,25 +269,56 @@ def ShifterOutGenerator(dut):
                 onetime = 1
         yield
 
+def CascadingShifterGenerator(dut):
+    cnt1 = 0
+    cnt2 = 0
+    for cycle in range(1000):
+
+        if cycle % 2 != 0:
+            if cnt1 < 10:
+                cnt1 = cnt1 + 1
+            else:
+                cnt1 = 0
+                yield dut.sck.eq(~dut.sck)
+
+        if cnt2 < 20:
+            cnt2 = cnt2 + 1
+        else:
+            cnt2 = 0
+            yield dut.si.eq(randrange(2))
+                
+                
+        if cycle > 2 and cycle < 4:
+            yield dut.start.eq(1)
+
+        yield
+
+
 if __name__ == "__main__":
     #d = EdgeDetector()
     # print(verilog.convert(EdgeDetector()))
     # print(verilog.convert(EdgeDetector(), ios = {d.s}))
     #run_simulation(d, generator(d), clocks={"sys": 10}, vcd_name="EdgeDetector.vcd")
-    
+
     #d = Debouncer()
     #run_simulation(d, generator(d), clocks={"sys": 10}, vcd_name="Debouncer.vcd")
 
     #t = TestEdgeDetector()
     #run_simulation(t, generator(t), clocks={"sys": 10}, vcd_name="TestEdgeDetector.vcd")
-    
+
     #t = ShifterIn()
     #print(verilog.convert(ShifterIn()))
     #run_simulation(t, ShifterInGenerator(t), clocks={"sys": 10}, vcd_name="ShifterIn.vcd")
     #os.system("gtkwave ShifterIn.vcd")
-    
-    t = ShifterOut()
+
+    #t = ShifterOut()
     #print(verilog.convert(ShifterOut()))
-    run_simulation(t, ShifterOutGenerator(t), clocks={"sys": 10}, vcd_name="ShifterOut.vcd")
-    os.system("gtkwave ShifterOut.vcd")    
+    #run_simulation(t, ShifterOutGenerator(t), clocks={"sys": 10}, vcd_name="ShifterOut.vcd")
+    #os.system("gtkwave ShifterOut.vcd")    
+
+    t = CascadingShifter()
+    #print(verilog.convert(CascadingShifter()))
+    run_simulation(t, CascadingShifterGenerator(t), clocks={"sys": 10}, vcd_name="CascadingShifter.vcd")
+    os.system("gtkwave CascadingShifter.vcd")
+
     
